928857d844ae52d8f80fe7c30cb73e9c439fc7bb
ByteBard <139046842+bytecanvas@users.noreply.github.com>
2024-05-12 12:32:36 +0545

    Update PS1.bash

--- PS1.bash
+++ PS1.bash
@@ -1,10 +1,39 @@
-# Define colors
-COLOR_USER="\[\033[38;5;214m\]"       # Orange for regular user
-COLOR_ROOT="\[\033[38;5;9m\]"          # Red for root user
-COLOR_RESET="\[\033[0m\]"              # Reset color
+# CUSTOM PS1
 
-# Define arrow symbol
-ARROW="➜"
+# Function to add colors
+add_color() {
+    echo -e "\[\e[$1m\]"
+}
 
-# Set PS1 prompt
-PS1="\n\[$COLOR_USER\]\w\n\[$COLOR_ROOT\]\h\n\[$COLOR_RESET\]\d \t\n\[$COLOR_USER\]$ARROW \[$COLOR_RESET\]"
+# Function to generate a random color code
+generate_random_color() {
+    echo -n "38;5;$((RANDOM % 256))"
+}
+
+# Prompt with color scheme 1: Green with random prompt color
+color_scheme_1() {
+    PS1="$(add_color "$(generate_random_color)")\w\n$(add_color "$(generate_random_color)")\A \d\n$(add_color "$(generate_random_color)")\h\n$(add_color '38;5;118')\[\e[0m\]➜ "
+}
+
+# Prompt with color scheme 2: Blue with random prompt color
+color_scheme_2() {
+    PS1="$(add_color "$(generate_random_color)")\w\n$(add_color "$(generate_random_color)")\A \d\n$(add_color "$(generate_random_color)")\h\n$(add_color '38;5;75')\[\e[0m\]➜ "
+}
+
+# Prompt with color scheme 3: Purple with random prompt color
+color_scheme_3() {
+    PS1="$(add_color "$(generate_random_color)")\w\n$(add_color "$(generate_random_color)")\A \d\n$(add_color "$(generate_random_color)")\h\n$(add_color '38;5;141')\[\e[0m\]➜ "
+}
+
+# Prompt with color scheme 4: Red with random prompt color
+color_scheme_4() {
+    PS1="$(add_color "$(generate_random_color)")\w\n$(add_color "$(generate_random_color)")\A \d\n$(add_color "$(generate_random_color)")\h\n$(add_color '38;5;160')\[\e[0m\]➜ "
+}
+
+# Prompt with color scheme 5: Cyan with random prompt color
+color_scheme_5() {
+    PS1="$(add_color "$(generate_random_color)")\w\n$(add_color "$(generate_random_color)")\A \d\n$(add_color "$(generate_random_color)")\h\n$(add_color '38;5;51')\[\e[0m\]➜ "
+}
+
+# Set the initial prompt with color scheme 1
+color_scheme_1
